01a0b1d6f8412b9c7bc104f58a672302560bc17b
HydroRICO <juanhanng1209@gmail.com>
2022-11-23 19:53:26 +0800

    Update test2.py

--- commando/test2.py
+++ commando/test2.py
@@ -11,7 +11,7 @@ class test2(commands.Cog):
     async def test2(self, ctx, arg):
         await ctx.send('1')
         
-        var = await self.bot.db_pool.fetch(f'SELECT {arg} FROM match WHERE matchid = str({ctx.channel.id})')
+        var = await self.bot.db_pool.fetch('SELECT matchstarted FROM match WHERE matchid = ?', (ctx.channel.id))
         await ctx.send(var[0])
 
         await ctx.send('2')
