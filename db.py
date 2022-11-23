b07b17a15edd640777befa3a97d95da571a3c50f
HydroRICO <juanhanng1209@gmail.com>
2022-11-23 15:44:19 +0800

    Update db.py

--- db.py
+++ db.py
@@ -4,5 +4,6 @@ async def db(ctx, arg):
     conn = await asyncpg.connect(os.getenv('DBURL'))
 
     var = await conn.fetch('SELECT $1 FROM match WHERE matchid = $2',arg ,ctx.channel.id)
+    print(var)
     return var
     await conn.close()
